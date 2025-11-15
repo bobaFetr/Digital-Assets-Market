dbc48eed9d7b919f73e74ed0a29a2303dad55206
Boris Petkov <boris.v.bobi.petkoff@gmail.com>
2025-11-15 13:43:23 +0200

    Added login, Sign UP pages

---
+++ .NetServer/.NetServer/Data/12.ReferralTable.cs
@@ -0,0 +1,19 @@
+using UserNameSpace.Data;
+
+namespace ReferralTable
+{
+    public class Referral
+    {
+        public int ReferralId { get; set; }
+        public int ReferrerId { get; set; }
+        public int ReferredId { get; set; }
+        public decimal BonusAmount { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public User Referrer { get; set; }
+        public User Referred { get; set; }
+
+
+        
+    }
+}
