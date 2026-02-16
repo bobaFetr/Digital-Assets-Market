fe0bce18550f3e40d4aecd55bc782cc5ff5f3f80
Boris Petkov <boris.v.bobi.petkoff@gmail.com>
2026-02-16 10:20:36 +0200

    new commit

--- MyWebApi/obj/Debug/net8.0/MyWebApi.AssemblyInfo.cs
+++ MyWebApi/obj/Debug/net8.0/MyWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15ad667a096fbebf4bc7eebb51ef7eabc9b88753")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5c6752f40ce4d86cd770053a8ef6e339c2a8691")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- NetServer.DAta1/obj/Debug/net8.0/NetServer.DAta1.AssemblyInfo.cs
+++ NetServer.DAta1/obj/Debug/net8.0/NetServer.DAta1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetServer.DAta1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15ad667a096fbebf4bc7eebb51ef7eabc9b88753")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5c6752f40ce4d86cd770053a8ef6e339c2a8691")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetServer.DAta1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetServer.DAta1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
